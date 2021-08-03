2f06caf348b8761c8109d54c18be6e86f978fc8b
Hamid <hamidur_rahman1@hotmail.co.uk>
2021-08-03 09:59:03 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'oooh secret page!'
+end
+
+get '/test' do
+  'oooh test page!'
+end
+
+get '/cat' do
+  "
+  <div style='border: 3px dashed red'>
+  <<img src='http://placekitten.com/500/500'>
+  </div>
+  "
+end
